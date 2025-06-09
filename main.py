efb8a277411a0e277bc45347ad6755c20071ee79
Tomas Corteggiano <tcorteggiano@gmail.com>
2025-06-09 18:40:28 -0300

    main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from numpy.random import RandomState
 from Benchmarking import Benchmark
 from Datasets import get_wine_dataset, label_encode, split_transpose
 from QDA import QDA, TensorizedQDA, FasterQDA, EfficientQDA, QDA_Chol1, QDA_Chol2, QDA_Chol3, TensorizedChol, EfficientChol
-from LDA import LDA
 
 X_full, y_full = get_wine_dataset()
 y_full_encoded = label_encode(y_full)
